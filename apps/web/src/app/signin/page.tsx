3672bd73e739d8f04252644c632c453a8a45be62
Nicolás Mengual Gómez <nicolas.mengual@promocionesfarma.com>
2023-06-24 21:22:26 +0200

    Small fixes
    Signed-off-by: Nicolás Mengual Gómez <nicolas.mengual@promocionesfarma.com>

--- apps/web/src/app/signin/page.tsx
+++ apps/web/src/app/signin/page.tsx
@@ -1,7 +1,12 @@
 import SignIn from "@src/application/signIn";
+import { Metadata } from "next";
 
 const SignInPage = () => {
   return <SignIn />;
+};
+
+export const metadata: Metadata = {
+  title: "EnvCat | Sign In",
 };
 
 export default SignInPage;
