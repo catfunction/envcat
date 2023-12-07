bb8d2bb24bef7fabdc2065d8a3b50cccd3d29de3
Nicolás Mengual <nmengualgomez@gmail.com>
2023-12-07 12:44:09 +0100

    Add generateMetadata function to ProjectPage and (#23)
    update project title in ProjectSwitcher
    
    Signed-off-by: Nicolás Mengual Gómez <nicolas.mengual@promocionesfarma.com>
    Co-authored-by: Nicolás Mengual Gómez <nicolas.mengual@promocionesfarma.com>

--- apps/web/src/app/(default)/[projectId]/page.tsx
+++ apps/web/src/app/(default)/[projectId]/page.tsx
@@ -1,7 +1,21 @@
 import Project from "@src/application/project/components/project";
+import type { Metadata } from "next";
+import useProject from "@src/application/project/hooks/useProject";
 
 const ProjectPage = ({ params }: { params: { projectId: string } }) => {
   return <Project projectId={params.projectId} />;
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { projectId: string };
+}): Promise<Metadata> {
+  const project = await useProject(params.projectId);
+
+  return {
+    title: `EnvCat | ${project.name}`,
+  };
+}
 
 export default ProjectPage;
